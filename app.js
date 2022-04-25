eda6aefba796d5e358782db19c197f1ffe70880e
courtsideKB <kbankhead1992.yahoo.com>
2022-04-24 23:07:07 -0700

    Refactor app.js and add module

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const res = require("express/lib/response");
+const date = require(__dirname + "/date.js");
 
 const app = express();
 app.use(bodyParser.urlencoded({
     extended: true
 }));
 
-let items = ["Buy food", "Cook food", "Eat food"];
-let workItems = [];
+const items = ["Buy food", "Cook food", "Eat food"];
+const workItems = [];
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
@@ -16,17 +17,7 @@ app.set("view engine", "ejs");
 /******* Home route get() *******/
 
 app.get("/", (req, res) => {
-
-    let today = new Date();
-
-    let options = {
-        weekday: "long",
-        day: "numeric",
-        month: "long"
-    };
-
-    let day = today.toLocaleDateString("en-US", options);
-
+   const day = date.getDate();
     res.render("list", {
         listTitle: day,
         newListItems: items
@@ -45,11 +36,11 @@ app.get("/work", (req, res) => {
 /******* About me route get() *******/
 
 app.get("/about", (req, res) => {
-    res.render("about"); 
+    res.render("about");
 });
 
 app.post("/", (req, res) => {
-    let item = req.body.newItem;
+    const item = req.body.newItem;
 
     if (req.body.list === "Work") {
         workItems.push(item);
